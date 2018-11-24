9c4d4d1b0c8dfe036f3930d44f1fc1876e3c05b0
shigezo <u.sake32@gmail.com>
2018-11-25 00:39:38 +0900

    feat: サーバーの作成

---
+++ MagicOnionInDocker.Server/MyFirstService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MagicOnion;
+using MagicOnion.Server;
+using MagicOnionInDocker.ServiceDefinition;
+
+namespace MagicOnionInDocker.Server
+{
+    // implement RPC service.
+    // inehrit ServiceBase<interface>, interface
+    public class MyFirstService : ServiceBase<IMyFirstService>, IMyFirstService
+    {
+        public async UnaryResult<int> SumAsync(int x, int y)
+        {
+            Logger.Debug($"Received:{x}, {y}");
+
+            return x + y;
+        }
+    }
+}
